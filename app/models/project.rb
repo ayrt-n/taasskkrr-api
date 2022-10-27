f2ea4fc1825e453c3692e6aa3230d972744edf3b
ayrton <ayrton.parkinson1@gmail.com>
2022-10-27 16:27:08 -0400

    Add custom #as_json for rendering projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,29 @@
 class Project < ApplicationRecord
   has_many :sections
   has_many :tasks, as: :taskable
+
+  def as_json(options = {})
+    super(
+      only: %i[id title],
+      include: {
+        tasks: {
+          include: {
+            sub_tasks: { only: %i[id title description priority due_date completed] }
+          },
+          only: %i[id title description priority due_date completed]
+        },
+        sections: {
+          include: {
+            tasks: {
+              include: {
+                sub_tasks: { only: %i[id title description priority due_date completed] }
+              },
+              only: %i[id title description priority due_date completed]
+            }
+          },
+          only: %i[id title]
+        }
+      }
+    )
+  end
 end
