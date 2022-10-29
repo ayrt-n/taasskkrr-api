00bf8a29fa16d7202a351f1bb8ef9fa903136bac
ayrton <ayrton.parkinson1@gmail.com>
2022-10-29 07:52:47 -0400

    Add routes for Sections, Tasks, and nested Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :projects, only: %i[show create update destroy]
+      resources :projects, only: %i[show create update destroy] do
+        resources :sections, only: %i[create]
+        resources :tasks, only: %i[create]
+      end
+
+      resources :sections, only: %i[update destroy] do
+        resources :tasks, only: %i[create]
+      end
+
+      resources :tasks, only: %i[update destroy] do
+        resource :tasks, only: %i[create]
+      end
     end
   end
 end
