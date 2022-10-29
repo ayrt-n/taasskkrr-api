c221536ad8a04f13453941c3310aea87fdbf2da5
ayrton <ayrton.parkinson1@gmail.com>
2022-10-29 07:53:06 -0400

    Add Task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   has_many :sub_tasks, as: :taskable, class_name: 'Task'
+  belongs_to :taskable, polymorphic: true
+
+  validates :title, presence: true
+  validate :not_nested_sub_sub_tasks
+
+  private
+
+  def not_nested_sub_sub_tasks
+    errors.add(:task, "can't be nested within a sub-task") if taskable.taskable_type == 'Task'
+  end
 end
