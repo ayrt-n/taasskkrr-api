205d4d5b0cca287cae4e7c12d4362d98eb4594ce
ayrton <ayrton.parkinson1@gmail.com>
2022-10-30 11:29:49 -0400

    Add request spec for registrations controller

---
+++ spec/controllers/api/v1/registrations_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::RegistrationsController, type: :request do
+  let(:user) { build_user }
+  let(:existing_user) { create_user }
+  let(:signup_url) { '/api/v1/signup' }
+
+  context 'When creating a new user' do
+    before do
+      post signup_url, params: {
+        user: {
+          email: user.email,
+          password: user.password
+        }
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns the user email' do
+      data = json
+      expect(data).to have_key('email')
+      expect(data['email']).to eq(user.email)
+    end
+  end
+
+  context 'When an email already exists' do
+    before do
+      post signup_url, params: {
+        user: {
+          email: existing_user.email,
+          password: existing_user.password
+        }
+      }
+    end
+
+    it 'returns 400' do
+      expect(response.status).to eq(400)
+    end
+  end
+end
