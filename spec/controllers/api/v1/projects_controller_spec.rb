14f541e1c61c3c443b4010eccb48260fa84bdad5
ayrton <ayrton.parkinson1@gmail.com>
2022-10-30 13:14:11 -0400

    Add request spec for authenticated project requests

---
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ProjectsController, type: :request do
+  let(:user) { create_user }
+  let(:project) { create_project }
+
+  context 'When fetching a project' do
+    before do
+      login_with_api(user)
+      get "/api/v1/projects/#{project.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns the user' do
+      data = json
+      expect(data['id']).to eq(project.id)
+      expect(data['title']).to eq(project.title)
+    end
+  end
+
+  context 'When a project is missing' do
+    before do
+      login_with_api(user)
+      get '/api/v1/projects/blank', headers: {
+        'Authorization': response.headers['Authorization']
+      }
+    end
+
+    it 'returns 404' do
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context 'When the Authorization header is missing' do
+    before do
+      get "/api/v1/projects/#{project.id}"
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+  end
+end
