3d6b96d6537fd1671f332bb140801bfefaf43b93
ayrton <ayrton.parkinson1@gmail.com>
2022-10-30 10:30:12 -0400

    Add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             defaults: { format: :json },
+             path: '',
+             path_names: {
+               sign_in: 'api/v1/login',
+               signout: 'api/v1/logout',
+               registration: 'api/v1/signup'
+             },
+             controllers: {
+               sessions: 'api/v1/sessions',
+               registrations: 'api/v1/registrations'
+             }
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :projects, only: %i[show create update destroy] do
