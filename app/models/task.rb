ac89f05881e45c4c5ac91654f290efede3362eaf
ayrton <ayrton.parkinson1@gmail.com>
2022-10-31 07:22:00 -0400

    Remove subtask and add belongs_to :user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,6 @@
 class Task < ApplicationRecord
-  has_many :sub_tasks, as: :taskable, class_name: 'Task'
   belongs_to :taskable, polymorphic: true
+  belongs_to :user
 
   validates :title, presence: true
-  validate :not_nested_sub_sub_tasks
-
-  private
-
-  def not_nested_sub_sub_tasks
-    return unless taskable_type == 'Task' && taskable.taskable_type == 'Task'
-
-    errors.add(:task, "can't be nested within a sub-task")
-  end
 end
