29c56449726cb3796d75f9181fcdae27b20d6980
ayrton <ayrton.parkinson1@gmail.com>
2022-10-31 07:22:17 -0400

    Add has_many associations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  has_many :tasks, dependent: :destroy
+  has_many :projects, dependent: :destroy
 end
