71d85da1d6b8a294908ea81ccf80e51de7c30327
ayrton <ayrton.parkinson1@gmail.com>
2022-10-31 07:24:27 -0400

    Increase JWT exp to 1 day

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,7 +23,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/api/v1/logout$}]
     ]
-    jwt.expiration_time = 15.minutes.to_i
+    jwt.expiration_time = 1.day.to_i
   end
 
   # ==> Controller configuration
