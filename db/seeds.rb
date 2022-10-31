a8e8072b218322945b42851f12d1a6f2d798c2b6
ayrton <ayrton.parkinson1@gmail.com>
2022-10-31 07:25:00 -0400

    Remove subtask from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-work_project = Project.create(title: 'Work')
-work_todo = work_project.tasks.create(title: 'General work task')
+user1 = User.create(
+  email: 'example@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+work_project = user1.projects.create(title: 'Work')
+work_project.tasks.create(title: 'General work task', user: user1)
 work_section = work_project.sections.create(title: 'Work Section')
-work_section_todo = work_section.tasks.create(title: 'Work section task')
+work_section.tasks.create(title: 'Work section task', user: user1)
