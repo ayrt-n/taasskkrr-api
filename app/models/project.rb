f8163ffabe828091eb7708d7337559537dc4f66a
ayrton <ayrton.parkinson1@gmail.com>
2022-10-31 07:23:09 -0400

    Remove subtask and add belongs_to :user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,27 +1,22 @@
 class Project < ApplicationRecord
+  belongs_to :user
   has_many :sections, dependent: :destroy
   has_many :tasks, as: :taskable, dependent: :destroy
 
   validates :title, presence: true
 
-  scope :include_all_tasks, -> { includes(tasks: [:sub_tasks], sections: { tasks: [:sub_tasks] }) }
+  scope :include_all_tasks, -> { includes(:tasks, sections: :tasks) }
 
   def as_json(options = {})
     super(
       only: %i[id title],
       include: {
         tasks: {
-          include: {
-            sub_tasks: { only: %i[id title description priority due_date completed] }
-          },
           only: %i[id title description priority due_date completed]
         },
         sections: {
           include: {
             tasks: {
-              include: {
-                sub_tasks: { only: %i[id title description priority due_date completed] }
-              },
               only: %i[id title description priority due_date completed]
             }
           },
