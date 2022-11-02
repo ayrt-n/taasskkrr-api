6749dd66a771ff965309eb9c1fd5ad2f26217ab5
ayrton <ayrton.parkinson1@gmail.com>
2022-11-02 06:58:42 -0400

    Add tests for #update

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::ProjectsController, type: :request do
+RSpec.describe 'ProjectsController#Show', type: :request do
   let(:user) { create(:user) }
   let(:project) { create(:project, user: user) }
   let(:unauthorized_user) { create(:user) }
@@ -52,6 +52,88 @@ RSpec.describe Api::V1::ProjectsController, type: :request do
   context 'When the Authorization header is missing' do
     before do
       get "/api/v1/projects/#{project.id}"
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+  end
+end
+
+RSpec.describe 'ProjectsController#Update', type: :request do
+  let(:user) { create(:user) }
+  let(:project) { create(:project, user: user) }
+  let(:unauthorized_user) { create(:user) }
+
+  context 'When user edits project belonging to the user' do
+    before do
+      login_with_api(user)
+      patch "/api/v1/projects/#{project.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        project: {
+          title: 'Updated Project'
+        }
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns the updated project' do
+      project.reload
+      expect(json['id']).to eq(project.id)
+      expect(json['title']).to eq(project.title)
+    end
+
+    it 'updates the project title' do
+      project.reload
+      expect(project.title).to eq('Updated Project')
+    end
+  end
+
+  context 'When user tries to update project not belonging to them' do
+    before do
+      login_with_api(unauthorized_user)
+      patch "/api/v1/projects/#{project.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        project: {
+          title: 'Updated Project'
+        }
+      }
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+  end
+
+  context 'When the project is missing' do
+    before do
+      login_with_api(user)
+      patch '/api/v1/projects/blank', headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        project: {
+          title: 'Updated Project'
+        }
+      }
+    end
+
+    it 'returns 404' do
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context 'When the Authorization header is missing' do
+    before do
+      patch "/api/v1/projects/#{project.id}", params: {
+        project: {
+          title: 'Updated Project'
+        }
+      }
     end
 
     it 'returns 401' do
