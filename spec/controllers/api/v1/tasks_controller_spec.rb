b8ab8f10d32a6b035dfc5e89a5774f57867b91ff
ayrton <ayrton.parkinson1@gmail.com>
2022-11-06 11:32:41 -0500

    Add tests for #destroy

--- spec/controllers/api/v1/tasks_controller_spec.rb
+++ spec/controllers/api/v1/tasks_controller_spec.rb
@@ -236,3 +236,58 @@ RSpec.describe 'TasksController#update', type: :request do
     end
   end
 end
+
+RSpec.describe 'TasksController#destroy', type: :request do
+  let(:user) { create(:user) }
+  let(:project) { create(:project, user: user) }
+  let(:task) { create(:task, user: user, taskable: project) }
+  let(:unauthorized_user) { create(:user) }
+
+  context 'When user deletes project belonging to user' do
+    before do
+      login_with_api(user)
+      delete "/api/v1/tasks/#{task.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'deletes the task' do
+      expect(project.tasks).not_to exist
+    end
+  end
+
+  context 'When user tries to delete project not belonging to them' do
+    before do
+      login_with_api(unauthorized_user)
+      delete "/api/v1/tasks/#{task.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+
+    it 'does not delete task' do
+      expect(project.tasks).to exist
+    end
+  end
+
+  context 'When the Authorization header is missing' do
+    before do
+      delete "/api/v1/tasks/#{task.id}"
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+
+    it 'does not delete task' do
+      expect(project.tasks).to exist
+    end
+  end
+end
