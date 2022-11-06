880d4c33172627eee2286e14b78ca6902b6d45c9
ayrton <ayrton.parkinson1@gmail.com>
2022-11-06 11:27:58 -0500

    Add tests for #update

--- spec/controllers/api/v1/tasks_controller_spec.rb
+++ spec/controllers/api/v1/tasks_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TasksController, type: :request do
+RSpec.describe 'TasksController#create', type: :request do
   let(:user) { create(:user) }
   let(:project) { create(:project, user: user) }
   let(:section) { create(:section, project: project) }
@@ -141,6 +141,98 @@ RSpec.describe Api::V1::TasksController, type: :request do
 
     it 'returns 401' do
       expect(response.status).to eq(401)
+    end
+  end
+end
+
+RSpec.describe 'TasksController#update', type: :request do
+  let(:user) { create(:user) }
+  let(:project) { create(:project, user: user) }
+  let(:task) { create(:task, user: user, taskable: project) }
+  let(:unauthorized_user) { create(:user) }
+
+  context 'When user edits task belonging to user' do
+    before do
+      login_with_api(user)
+      patch "/api/v1/tasks/#{task.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        task: {
+          title: 'Updated task'
+        }
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns the updated task' do
+      expect(json['id']).to eq(task.id)
+      expect(json['title']).to eq('Updated task')
+    end
+
+    it 'updates the task' do
+      task.reload
+      expect(task.title).to eq('Updated task')
+    end
+  end
+  
+  context 'When user tries to edit task not belonging to user' do
+    before do
+      login_with_api(unauthorized_user)
+      patch "/api/v1/tasks/#{task.id}", headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        task: {
+          title: 'Updated task'
+        }
+      }
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+
+    it 'does not update the task' do
+      task.reload
+      expect(task.title).to eq('A simple task')
+    end
+  end
+
+  context 'When task is missing' do
+    before do
+      login_with_api(user)
+      patch '/api/v1/tasks/blank', headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        task: {
+          title: 'Updated task'
+        }
+      }
+    end
+
+    it 'returns 404' do
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context 'When the Authorization header is missing' do
+    before do
+      patch "/api/v1/tasks/#{task.id}", params: {
+        task: {
+          title: 'Updated task'
+        }
+      }
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+
+    it 'does not update the task' do
+      task.reload
+      expect(task.title).to eq('A simple task')
     end
   end
 end
