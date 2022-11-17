69924d968c797ca91680a734bab4a1e3483c4741
ayrton <ayrton.parkinson1@gmail.com>
2022-11-17 07:28:03 -0500

    Replace render json with #render_resource

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ module Api
       private
 
       def respond_with(resource, _opts = {})
-        render json: resource
+        render_resource(resource)
       end
 
       def respond_to_on_destroy
