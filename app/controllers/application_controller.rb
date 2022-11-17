f9818fab7370fc1b3ea2c229a6b0841294d1c5c8
ayrton <ayrton.parkinson1@gmail.com>
2022-11-17 07:27:47 -0500

    Adjust json error message format for consistency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,31 +11,31 @@ class ApplicationController < ActionController::API
 
   def validation_error(resource)
     render json: {
-      errors: [
-        {
-          status: '422',
-          title: 'Unprocessable Entity',
-          details: resource.errors
-        }
-      ]
+      error: {
+        status: '422',
+        title: 'Unprocessable Entity',
+        details: resource.errors.full_messages
+      }
     }, status: :unprocessable_entity
   end
 
   def not_found
     render json: {
-      errors: [
+      error: [
         status: '404',
-        title: 'Not Found'
+        title: 'Not Found',
+        details: 'The requested page could not be found.'
       ]
     }, status: :not_found
   end
 
   def access_denied
     render json: {
-      errors: [
+      error: {
         status: '401',
-        title: 'Access denied'
-      ]
+        title: 'Access denied',
+        details: 'You do not have the correct permissions.'
+      }
     }, status: :unauthorized
   end
 end
