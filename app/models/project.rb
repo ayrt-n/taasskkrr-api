9006121b718f88386caff1265cf1af05570bc708
ayrton <ayrton.parkinson1@gmail.com>
2022-11-26 09:52:06 -0500

    Add inbox flag to #as_json render

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,17 +7,17 @@ class Project < ApplicationRecord
 
   scope :include_all_tasks, -> { includes(:tasks, sections: :tasks) }
 
-  def as_json(options = {})
+  def as_json(_options = {})
     super(
-      only: %i[id title],
+      only: %i[id title inbox],
       include: {
         tasks: {
-          only: %i[id title description priority due_date completed]
+          only: %i[id title description priority due_date status]
         },
         sections: {
           include: {
             tasks: {
-              only: %i[id title description priority due_date completed]
+              only: %i[id title description priority due_date status]
             }
           },
           only: %i[id title]
