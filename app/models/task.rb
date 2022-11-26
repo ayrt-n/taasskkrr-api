7cbb0b48944308c4af5fbd0934b10bcea1b3a68a
ayrton <ayrton.parkinson1@gmail.com>
2022-11-26 09:51:47 -0500

    Add custom #as_json for rendering tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,10 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
+
+  def as_json(_options = {})
+    super(
+      only: %i[id title description priority due_date status]
+    )
+  end
 end
