1d52562ccb5f4a1e0e7927a4fd226812585ea219
ayrton <ayrton.parkinson1@gmail.com>
2022-11-27 11:36:36 -0500

    Add autoload for CustomFailureApp

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module ToDoListsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Autoload custom devise failure
+    config.autoload_paths << Rails.root.join('lib/devise/failure')
   end
 end
