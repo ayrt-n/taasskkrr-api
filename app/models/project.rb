9afdabeca27dcfbf586042504020818f4cbb2849
ayrton <ayrton.parkinson1@gmail.com>
2022-11-29 07:01:35 -0500

    Simplify custom as_json to prevent unnecessary includes

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,22 +7,8 @@ class Project < ApplicationRecord
 
   scope :include_all_tasks, -> { includes(:tasks, sections: :tasks) }
 
-  def as_json(_options = {})
-    super(
-      only: %i[id title inbox],
-      include: {
-        tasks: {
-          only: %i[id title description priority due_date status]
-        },
-        sections: {
-          include: {
-            tasks: {
-              only: %i[id title description priority due_date status]
-            }
-          },
-          only: %i[id title]
-        }
-      }
-    )
+  def as_json(options = {})
+    options[:only] = %i[id title inbox] unless options[:only]
+    super(options)
   end
 end
