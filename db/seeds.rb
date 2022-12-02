45d73cea529579b1235fc1912e1c25fade420853
ayrton <ayrton.parkinson1@gmail.com>
2022-12-02 07:17:39 -0500

    Add new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,48 @@ user1 = User.create(
 work_project = user1.projects.create(title: 'Work')
 work_project.tasks.create(title: 'General work task',
                           description: 'Just a general work task that needs to be done',
-                          due_date: Date.current + 1.week,
-                          user: user1)
+                          due_date: Date.current + 1.week)
 work_project.tasks.create(title: 'General work task',
                           description: 'Just a general work task that needs to be done',
                           due_date: Date.current + 1.week,
                           status: 1,
-                          priority: 2,
-                          user: user1)
+                          priority: 2)
 work_project.tasks.create(title: 'General work task',
                           description: 'Just a general work task that needs to be done',
                           due_date: Date.current + 1.week,
-                          priority: 1,
-                          user: user1)
+                          priority: 1)
 work_section = work_project.sections.create(title: 'Work Section')
 work_section.tasks.create(title: 'General work section task',
                           description: 'Just a general work section task as part of the section',
                           due_date: Date.current + 1.week,
-                          user: user1)
+                          project_id: work_section.project_id)
 work_section.tasks.create(title: 'General work section task',
                           description: 'Just a general work section task as part of the section',
                           due_date: Date.current + 1.week,
-                          user: user1)
+                          project_id: work_section.project_id)
+work_section2 = work_project.sections.create(title: 'Another!!!')
+work_section2.tasks.create(title: 'General work section task',
+  description: 'Just a general work section task as part of the section',
+  due_date: Date.current + 1.week,
+  project_id: work_section.project_id)
+work_section2.tasks.create(title: 'General work section task',
+  description: 'Just a general work section task as part of the section',
+  due_date: Date.current + 1.week,
+  project_id: work_section.project_id)
+work_section2.tasks.create(title: 'General work section task',
+  description: 'Just a general work section task as part of the section',
+  due_date: Date.current + 1.week,
+  project_id: work_section.project_id)
+work_section3 = work_project.sections.create(title: 'Another!!!')
+work_section3.tasks.create(title: 'General work section task',
+  description: 'Just a general work section task as part of the section',
+  due_date: Date.current + 1.week,
+  project_id: work_section.project_id)
+work_section3.tasks.create(title: 'General work section task',
+  description: 'Just a general work section task as part of the section',
+  due_date: Date.current + 1.week,
+  project_id: work_section.project_id)
+work_section3.tasks.create(title: 'General work section task',
+  description: 'Just a general work section task as part of the section',
+  due_date: Date.current + 1.week,
+  project_id: work_section.project_id)
