7d5d56377aa311897233805e8d7ad4d1a0bb9d4a
ayrton <ayrton.parkinson1@gmail.com>
2022-12-02 07:15:44 -0500

    Replace direct has_many tasks association with has_many through projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
-  has_many :tasks, dependent: :destroy
   has_many :projects, dependent: :destroy
+  has_many :tasks, through: :projects, dependent: :destroy
 
   before_create :build_inbox
 
