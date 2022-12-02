df86ae3a3871f6758990f251314fd769342c9bd1
ayrton <ayrton.parkinson1@gmail.com>
2022-12-02 07:13:07 -0500

    Split out project tasks and all tasks association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,18 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :sections, dependent: :destroy
-  has_many :tasks, as: :taskable, dependent: :destroy
+  has_many :tasks, dependent: :destroy
+  has_many :project_tasks, -> { where section_id: nil }, class_name: 'Task'
 
   validates :title, presence: true
 
   scope :include_all_tasks, -> { includes(:tasks, sections: :tasks) }
 
   def as_json(options = {})
+    # Set options :only as default if not supplied
     options[:only] = %i[id title inbox] unless options[:only]
-    super(options)
+
+    # Rename 'project_tasks' key to just be 'tasks'
+    super(options).tap { |hash| hash['tasks'] = hash.delete('project_tasks') }
   end
 end
