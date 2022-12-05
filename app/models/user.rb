3ac8ad5f72d14a7761173d03dbb332316cfa1a4c
ayrton <ayrton.parkinson1@gmail.com>
2022-12-05 07:43:12 -0500

    Add :confirmable to devise user options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
