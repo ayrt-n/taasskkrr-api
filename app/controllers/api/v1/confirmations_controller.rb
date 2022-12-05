81d0179aab54a684cdf4d369f90e97460d501767
ayrton <ayrton.parkinson1@gmail.com>
2022-12-05 07:46:01 -0500

    Add custom ConfirmationsController#create

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -3,6 +3,13 @@ module Api
     class ConfirmationsController < Devise::ConfirmationsController
       respond_to :json
 
+      def create
+        self.resource = resource_class.send_confirmation_instructions(resource_params)
+        yield resource if block_given?
+
+        render_resource(resource)
+      end
+
       def show
         self.resource = resource_class.confirm_by_token(params[:confirmation_token])
         yield resource if block_given?
