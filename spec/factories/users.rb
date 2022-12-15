d59f60fdf2f8e14e18b5b4fd256bcf3269024aa9
ayrton <ayrton.parkinson1@gmail.com>
2022-12-15 07:20:30 -0500

    Add after build and create callback to confirm the users email via devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+
+    after(:build) { |u| u.skip_confirmation_notification! }
+    after(:create) { |u| u.confirm }
   end
 end
