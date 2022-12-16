8a50d6341cd4ecb4cf063e19da462519a04eda01
ayrton <ayrton.parkinson1@gmail.com>
2022-12-16 15:32:42 -0500

    Fix issue with development cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.development?
-      origins 'https://localhost:3000'
+      origins 'http://localhost:3000'
     else
       origins 'https://ayrt-n.github.io'
     end
