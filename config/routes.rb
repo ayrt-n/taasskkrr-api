951db099148f1624ca270ea6d58dec2c7663c0e1
ayrton <ayrton.parkinson1@gmail.com>
2022-12-30 09:59:25 -0500

    Add route for PingsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # Devise Routes
   devise_for :users,
              defaults: { format: :json },
              path: '',
@@ -16,18 +17,25 @@ Rails.application.routes.draw do
                passwords: 'api/v1/passwords'
              }
 
+  # API - Version 1 Routes
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      # Project-Specific Routes
       resources :projects, only: %i[index show create update destroy] do
         resources :sections, only: %i[create]
         resources :tasks, only: %i[create]
       end
 
+      # Section-Specific Routes
       resources :sections, only: %i[update destroy] do
         resources :tasks, only: %i[create]
       end
 
+      # Task-Specific Routes
       resources :tasks, only: %i[index update destroy]
     end
   end
+
+  # Route to ping heroku server to wake up
+  get '/ping', to: 'pings#ping', defaults: { format: :json }
 end
