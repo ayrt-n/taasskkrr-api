d1a1c318a395770af928d56ba34b900e670a6cf1
ayrton <ayrton.parkinson1@gmail.com>
2023-01-10 06:41:03 -0500

    Refactor Tasks spec to split between http verbs

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,143 +1,145 @@
 require 'rails_helper'
 
-RSpec.describe 'TasksController#create', type: :request do
-  let(:user) { create(:user) }
-  let(:project) { create(:project, user: user) }
-  let(:section) { create(:section, project: project) }
-  let(:unauthorized_user) { create(:user) }
+RSpec.describe 'Tasks', type: :request do
+  context '/POST Tasks' do
+    let(:user) { create(:user) }
+    let(:project) { create(:project, user: user) }
+    let(:section) { create(:section, project: project) }
+    let(:unauthorized_user) { create(:user) }
 
-  context 'When user creates task for project that belongs to them' do
-    before do
-      login_with_api(user)
-      post "/api/v1/projects/#{project.id}/tasks", headers: {
-        'Authorization': response.headers['Authorization']
-      }, params: {
-        task: {
-          title: 'Test Task'
+    context 'When user creates task for project that belongs to them' do
+      before do
+        login_with_api(user)
+        post "/api/v1/projects/#{project.id}/tasks", headers: {
+          'Authorization': response.headers['Authorization']
+        }, params: {
+          task: {
+            title: 'Test Task'
+          }
         }
-      }
+      end
+
+      it 'returns 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'adds task to users project' do
+        new_task_id = json['id']
+        user_project_tasks = project.tasks
+        expect(user_project_tasks.ids).to include(new_task_id)
+      end
     end
 
-    it 'returns 200' do
-      expect(response.status).to eq(200)
-    end
-
-    it 'adds task to users project' do
-      new_task_id = json['id']
-      user_project_tasks = project.tasks
-      expect(user_project_tasks.ids).to include(new_task_id)
-    end
-  end
-
-  context 'When user creates task for section that belongs to them' do
-    before do
-      login_with_api(user)
-      post "/api/v1/sections/#{section.id}/tasks", headers: {
-        'Authorization': response.headers['Authorization']
-      }, params: {
-        task: {
-          title: 'Test Task'
+    context 'When user creates task for section that belongs to them' do
+      before do
+        login_with_api(user)
+        post "/api/v1/sections/#{section.id}/tasks", headers: {
+          'Authorization': response.headers['Authorization']
+        }, params: {
+          task: {
+            title: 'Test Task'
+          }
         }
-      }
+      end
+
+      it 'returns 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'adds task to users project section' do
+        new_task_id = json['id']
+        user_section_tasks = section.tasks
+        expect(user_section_tasks.ids).to include(new_task_id)
+      end
     end
 
-    it 'returns 200' do
-      expect(response.status).to eq(200)
-    end
-
-    it 'adds task to users project section' do
-      new_task_id = json['id']
-      user_section_tasks = section.tasks
-      expect(user_section_tasks.ids).to include(new_task_id)
-    end
-  end
-
-  context 'When user tries to create task for project that does not belong to them' do
-    before do
-      login_with_api(unauthorized_user)
-      post "/api/v1/projects/#{project.id}/tasks", headers: {
-        'Authorization': response.headers['Authorization']
-      }, params: {
-        task: {
-          title: 'Test Task'
+    context 'When user tries to create task for project that does not belong to them' do
+      before do
+        login_with_api(unauthorized_user)
+        post "/api/v1/projects/#{project.id}/tasks", headers: {
+          'Authorization': response.headers['Authorization']
+        }, params: {
+          task: {
+            title: 'Test Task'
+          }
         }
-      }
+      end
+
+      it 'returns 401' do
+        expect(response.status).to eq(401)
+      end
     end
 
-    it 'returns 401' do
-      expect(response.status).to eq(401)
-    end
-  end
-
-  context 'When user tries to create task for section that does not belong to them' do
-    before do
-      login_with_api(unauthorized_user)
-      post "/api/v1/sections/#{section.id}/tasks", headers: {
-        'Authorization': response.headers['Authorization']
-      }, params: {
-        task: {
-          title: 'Test Task'
+    context 'When user tries to create task for section that does not belong to them' do
+      before do
+        login_with_api(unauthorized_user)
+        post "/api/v1/sections/#{section.id}/tasks", headers: {
+          'Authorization': response.headers['Authorization']
+        }, params: {
+          task: {
+            title: 'Test Task'
+          }
         }
-      }
+      end
+
+      it 'returns 401' do
+        expect(response.status).to eq(401)
+      end
     end
 
-    it 'returns 401' do
-      expect(response.status).to eq(401)
-    end
-  end
-
-  context 'When user tries to create task for project that is missing' do
-    before do
-      login_with_api(user)
-      post '/api/v1/projects/blank/tasks', headers: {
-        'Authorization': response.headers['Authorization']
-      }, params: {
-        section: {
-          title: 'Test Task'
+    context 'When user tries to create task for project that is missing' do
+      before do
+        login_with_api(user)
+        post '/api/v1/projects/blank/tasks', headers: {
+          'Authorization': response.headers['Authorization']
+        }, params: {
+          section: {
+            title: 'Test Task'
+          }
         }
-      }
+      end
+
+      it 'returns 404' do
+        expect(response.status).to eq(404)
+        expect(json).to include('error')
+      end
     end
 
-    it 'returns 404' do
-      expect(response.status).to eq(404)
-      expect(json).to include('error')
-    end
-  end
-
-  context 'When user tries to create task for section that is missing' do
-    before do
-      login_with_api(user)
-      post '/api/v1/sections/blank/tasks', headers: {
-        'Authorization': response.headers['Authorization']
-      }, params: {
-        section: {
-          title: 'Test Task'
+    context 'When user tries to create task for section that is missing' do
+      before do
+        login_with_api(user)
+        post '/api/v1/sections/blank/tasks', headers: {
+          'Authorization': response.headers['Authorization']
+        }, params: {
+          section: {
+            title: 'Test Task'
+          }
         }
-      }
+      end
+
+      it 'returns 404' do
+        expect(response.status).to eq(404)
+        expect(json).to include('error')
+      end
     end
 
-    it 'returns 404' do
-      expect(response.status).to eq(404)
-      expect(json).to include('error')
-    end
-  end
-
-  context 'When the Authorization header is missing' do
-    before do
-      post "/api/v1/projects/#{project.id}/tasks", params: {
-        section: {
-          title: 'Test Task'
+    context 'When the Authorization header is missing' do
+      before do
+        post "/api/v1/projects/#{project.id}/tasks", params: {
+          section: {
+            title: 'Test Task'
+          }
         }
-      }
-    end
+      end
 
-    it 'returns 401' do
-      expect(response.status).to eq(401)
+      it 'returns 401' do
+        expect(response.status).to eq(401)
+      end
     end
   end
 end
 
-RSpec.describe 'TasksController#update', type: :request do
+context '/PATCH Tasks' do
   let(:user) { create(:user) }
   let(:project) { create(:project, user: user) }
   let(:task) { create(:task, project: project) }
@@ -229,7 +231,7 @@ RSpec.describe 'TasksController#update', type: :request do
   end
 end
 
-RSpec.describe 'TasksController#destroy', type: :request do
+context '/DELETE Tasks' do
   let(:user) { create(:user) }
   let(:project) { create(:project, user: user) }
   let(:task) { create(:task, project: project) }
