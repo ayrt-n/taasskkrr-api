6fb553d6d926b79cef082e7fa7af9d79c56f2557
ayrton <ayrton.parkinson1@gmail.com>
2023-01-10 06:40:38 -0500

    Refactor formatting of spec

--- spec/requests/api/v1/pings_spec.rb
+++ spec/requests/api/v1/pings_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Pings#Ping', type: :request do
-  it 'returns 200' do
-    get '/api/v1/ping'
-    expect(response.status).to eq(200)
+RSpec.describe 'Pings', type: :request do
+  context '/GET Ping' do
+    it 'returns 200' do
+      get '/api/v1/ping'
+      expect(response.status).to eq(200)
+    end
   end
 end
