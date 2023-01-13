d85123638a2a09eecf285923725c1663702499a0
ayrton <ayrton.parkinson1@gmail.com>
2023-01-13 11:25:04 -0500

    Add explanatory comments

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
 
+    # Skip sending email confirmation and manually confirm the user account
     after(:build) { |u| u.skip_confirmation_notification! }
     after(:create) { |u| u.confirm }
   end
